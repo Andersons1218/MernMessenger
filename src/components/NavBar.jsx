c4179c1bf1e7ff6fdb52023ba85870382a5dbd0a
andersons1218 <andersons1218@gmail.com>
2022-07-01 15:32:54 -0400

    MERN infrastructure started and added tailwind css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,9 +12,9 @@ export default function NavBar({ user, setUser }) {
 }
     return(
         <nav>
-            <Link to="/orders">Order History</Link>
+            <Link to="/booking">Booking</Link>
             &nbsp; | &nbsp;
-            <Link to="/orders/new">New Order</Link>
+            <Link to="/booking/checkout">Check out</Link>
             &nbsp; | &nbsp;
             Welcome, { user.name }
             &nbsp; | &nbsp;
