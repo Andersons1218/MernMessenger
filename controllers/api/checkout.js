e612907b49a9e0338bedac8d853619f3cf791fbf
mileslathan <mlathan.cn@gmail.com>
2022-07-05 15:10:08 -0400

    changes to packages/checkout controllers.

--- controllers/api/checkout.js
+++ controllers/api/checkout.js
@@ -1,5 +1,5 @@
-const Package = require('../../models/order');
-const Item = require('../../models/packages');
+const Checkout = require('../../models/checkout');
+const Package = require('../../models/packages');
 
 module.exports = {
   cart,
@@ -10,13 +10,13 @@ module.exports = {
 
 // A cart is the unpaid order for a user
 async function cart(req, res) {
-  const cart = await Package.getCart(req.user._id);
+  const cart = await Checkout.getCart(req.user._id);
   res.json(cart);
 }
 
 // Add an item to the cart
 async function addToCart(req, res) {
-  const cart = await Order.getCart(req.user._id);
+  const cart = await Checkout.getCart(req.user._id);
   // The promise resolves to the document, which we already have
   // in the cart variable, so no need to create another variable...
   await cart.addItemToCart(req.params.id); 
@@ -25,14 +25,14 @@ async function addToCart(req, res) {
 
 // Updates an item in the cart's qty
 async function setItemQtyInCart(req, res) {
-  const cart = await Order.getCart(req.user._id);
+  const cart = await Checkout.getCart(req.user._id);
   await cart.setItemQty(req.body.itemId, req.body.newQty); 
   res.json(cart);
 }
 
 // Update the cart's isPaid property to true
 async function checkout(req, res) {
-  const cart = await Order.getCart(req.user._id);
+  const cart = await Checkout.getCart(req.user._id);
   cart.isPaid = true;
   await cart.save(); 
   res.json(cart);
