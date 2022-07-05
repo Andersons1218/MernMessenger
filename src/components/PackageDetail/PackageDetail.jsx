28f8d0d93765b69a10e2f69c94f24e892a7a5681
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-07-05 14:55:48 -0400

    add BookingPage

--- src/components/PackageDetail/PackageDetail.jsx
+++ src/components/PackageDetail/PackageDetail.jsx
@@ -1,22 +1,10 @@
 import { useParams } from 'react-router-dom'
+import PackageListItem from '../PackageListItem/PackageListItem';
 
-
-export default function PackageDetail({Packages}) {
-    
-  let { packageId } = useParams()
-
-  let selectedPackage = Packages.find(pkg => pkg.location === packageId)
+export default function PackageDetail() {
 
   return (
-      <>
-     <h1>Package Details:</h1>
-     <h2>{selectedPackage.location}</h2>
-     <p>Hotel: {selectedPackage.hotel}<br/>
-     Rating: {selectedPackage.rating}<br/>
-     Flight: {selectedPackage.flight}<br/>
-     Price: {selectedPackage.price}<br/>
-     Car: {selectedPackage.car}<br/>
-     </p>
-     </>
+    <h1>Package Details</h1>
+
     );
-  }
+}
