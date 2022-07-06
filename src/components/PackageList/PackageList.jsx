6f100a9a007929e9b03557ac036d0beae327fd10
mileslathan <mlathan.cn@gmail.com>
2022-07-06 10:46:58 -0400

    Passed down data from db to Booking and packages page.

--- src/components/PackageList/PackageList.jsx
+++ src/components/PackageList/PackageList.jsx
@@ -1,17 +1,17 @@
 import PackageListItem from "../PackageListItem/PackageListItem";
 
-export default function PackageList() {
-  const packages = packages.map(item =>
-    <PackageListItem 
-      key={packages._id}
-      packages={packages}
-      handleAddToOrder={handleAddToOrder}
-    />
-  );
+export default function PackageList({packages}) {
+  // console.log(`here are the packages from pacakage list ${packages[0]._id}`)
+  
   return (
+    <>
     <main>
-      {packages}
+    {/* {console.log(packages)} */}
+    { packages.map(pkg => {
+      return <PackageListItem key={pkg._id} pkg={pkg} />
+    })}
     </main>
+      </>
   )
 }
 
