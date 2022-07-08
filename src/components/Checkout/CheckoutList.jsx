b5a4e124cfd6f020f380e76a5c6d59e12fa8dc40
Anderson Santana <104103694+Andersons1218@users.noreply.github.com>
2022-07-08 10:29:04 -0400

    remove items from checkout

--- src/components/Checkout/CheckoutList.jsx
+++ src/components/Checkout/CheckoutList.jsx
@@ -4,7 +4,7 @@ export default function CheckoutList({checkout, setCheckout}) {
     return(
         <>
     { checkout.map(pkg => {
-      return <CheckoutListItem key={pkg._id} pkg={pkg} />
+      return <CheckoutListItem key={pkg._id} pkg={pkg} checkout={checkout} setCheckout={setCheckout} />
     })}
         </>
     )
--- src/components/Checkout/CheckoutListItem.jsx
+++ src/components/Checkout/CheckoutListItem.jsx
@@ -1,4 +1,15 @@
-export default function CheckoutListItem({pkg, setCheckout}){
+export default function CheckoutListItem({pkg, setCheckout, checkout}){
+
+    //creat fuction to remove package from checkout
+    const removepkg = () => {
+        setCheckout(current =>
+          current.filter(pkgs => {
+            // 👇️ remove object that has id equal to 2
+            return pkgs !== pkg;
+          }),
+        );
+      };
+
     return(
         <>
         <div style={{'background': `url(${pkg.location_img}) no-repeat center center`, 'WebkitBackgroundSize': '300px'}}className="card-banner">       
@@ -7,7 +18,7 @@ export default function CheckoutListItem({pkg, setCheckout}){
         <h2>Flight: {pkg.flight}</h2>
         <h2>Car Rental: {pkg.car}</h2>
         <h2>Quantity: {pkg.persons}</h2>
-        <button>Remove</button>
+        <button onClick={removepkg}>Remove</button>
         </div>
         </>    
     )
