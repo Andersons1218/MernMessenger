1b350a4c22e3dc3786fc3ec1e4cf6d66686066ce
Anderson Santana <104103694+Andersons1218@users.noreply.github.com>
2022-07-11 09:57:25 -0400

    styling

--- src/components/Checkout/CheckoutTotal.jsx
+++ src/components/Checkout/CheckoutTotal.jsx
@@ -4,10 +4,7 @@ export default function CheckoutTotal({addToProf, checkout}){
         <>
         <aside>
             <ul>
-                <li>Package One</li>
-                <li>Package Two</li>
-                <li>Package Three</li>
-                <li>Package Four</li>
+                
             </ul>
             <button onClick={addToProf}> Submit Order</button>
         </aside>
